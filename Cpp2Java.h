3ee2d531ec1b35d72a2e42f675fc8cbb6cc7337b
Kai Gustafson <kjgustafson1996@pointloma.edu>
2017-02-11 20:49:22 -0800

    massive changes

--- Cpp2Java.h
+++ Cpp2Java.h
@@ -5,29 +5,109 @@
 
 using namespace std;
 
-static class Cpp2Java
+class Commands
 {
 public:
+	vector<string> commands;
+	int instanceCounter;
+};
+Commands c;
 
-	//methods declared here
-	void removeAll();
+class Polygon
+{
+public:
+	void addPoint(int x, int y);
+};
+void Polygon::addPoint(int x, int y)
+{
 
+}
+
+class JComponent
+{
+	friend class Cpp2Java;
+public:
+	virtual void add(JComponent & jc);
+	ofstream file;
+protected:
+	void setInstanceName();
+	string instanceName;
 
 };
+void JComponent::add(JComponent & jc)
+{
+	c.commands.push_back("add," + instanceName + "," + jc.instanceName);
+}
+void JComponent::setInstanceName()
+{
+	instanceName = to_string(c.instanceCounter);
+	c.instanceCounter++;
+}
 
-// methods written down here
+class JPanel : public JComponent
+{
+public:
+	JPanel();
+};
+JPanel::JPanel()
+{
+	setInstanceName();
+	c.commands.push_back("instantiate,JPanel," + instanceName);
+}
 
+class JLabel : public JComponent
+{
+public:
+	JLabel(string s);
+	void setText(string s);
+};
+JLabel::JLabel(string s)
+{
+	setInstanceName();
+	c.commands.push_back("instantiate,JLabel," + s + "," + instanceName);
+}
+void JLabel::setText(string s)
+{
+	c.commands.push_back("setText," + s + "," + instanceName);
+}
+
+class Cpp2Java
+{
+public:
+	Cpp2Java();
+	void removeAll();
+	void finish();
+	void setLayout(string s);
+	ofstream file;
+private:
+	JComponent * JC;
+};
+Cpp2Java::Cpp2Java()
+{
+	JC = new JComponent();
+}
 void Cpp2Java::removeAll()
 {
-	ofstream file;
 	string removeAll;
-	file.open("text.txt");
+	file.open("text.csv");
 	file.clear();
 
 	removeAll = "removeAll \n";
 
 	file << removeAll;
 
+}
+void Cpp2Java::finish()
+{
+	for (string s : c.commands)
+	{
+		file << s + "\n";
+	}
+
 	file.close();
+}
+void Cpp2Java::setLayout(string s)
+{
+	c.commands.push_back("setLayout," + s);
 }
 
