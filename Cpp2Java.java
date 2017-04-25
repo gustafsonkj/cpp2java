def4e2f97903c6f7d5a9e3f4e6e8c519eeceb9e9
ZacLindsey <zaclindsey1996@pointloma.edu>
2017-04-25 14:11:15 -0700

    fixed glitch (missing parameter)

--- Cpp2Java.java
+++ Cpp2Java.java
@@ -269,7 +269,7 @@ public class Cpp2Java extends JFrame { //One-JFrame setup
                             break;
                             
                             case "JRadioButton":
-                              comps.add(ID, new DynamicJRadioButton(line[4]));
+                              comps.add(ID, new DynamicJRadioButton(line[4],ID));
                             break;
                             
                             case "JCheckBox":
